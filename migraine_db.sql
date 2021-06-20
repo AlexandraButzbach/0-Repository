052a98e007360ebc2052278e0b3534f4292fdeb5
AlexandraButzbach <butzbacha@bzz.ch>
2021-06-20 17:15:59 +0200

    Anpassung Format SQL Datei

--- migraine_db.sql
+++ migraine_db.sql
@@ -1,4 +1,9 @@
+show databases;
 use migraine_db;
+show tables;
+select * from migraine;
+
+
 CREATE TABLE migraine (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     migraine_datum VARCHAR(50),
@@ -6,4 +11,6 @@ CREATE TABLE migraine (
     migraine_ende VARCHAR(50),
     migraine_dauer VARCHAR(50),
     migraine_intensitaet VARCHAR(50)
-);
+)
+
+
